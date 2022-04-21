6b6cb4913ba0a707077ab584b89f1ca71bb36d7b
punyatoya16 <69297551+punyatoya16@users.noreply.github.com>
2022-04-21 19:32:33 +0530

    Add files via upload
    this is a simple madlib

---
+++ madlib.py
@@ -0,0 +1,6 @@
+#MADLIB
+adj=input("adjectictive:")
+cel=input("hobby:")
+famous=input('famous person:')
+madlib=f"computer programming is so {adj} i love{cel} stay hydrated like yu are {famous}"
+print(madlib)
